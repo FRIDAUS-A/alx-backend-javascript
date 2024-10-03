3cf023da5886b2b22398e8bb5ecdfeb6980c140d
FRIDAUS <fridausokoya@gmail.com>
2024-10-03 10:39:04 +0100

    iterate through string

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let iterateString = '';
+  let idx = 0;
+  for (const name of reportWithIterator) {
+    iterateString = iterateString.concat(name);
+    if (idx !== reportWithIterator.length - 1) {
+      iterateString = iterateString.concat(' | ');
+    }
+    idx += 1;
+  }
+  return iterateString
+}
