585727dd0f72e5ee5dd56bff9682749a84ba7e57
FRIDAUS <fridausokoya@gmail.com>
2024-10-04 22:44:39 +0100

    Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,13 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   get sqft() {
