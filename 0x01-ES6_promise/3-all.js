a3ecd591d93141b5b7e5933e1542c2e3e89ae35b
FRIDAUS <fridausokoya@gmail.com>
2024-10-05 18:09:13 +0100

    await

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function handleProfileSignup() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+}
