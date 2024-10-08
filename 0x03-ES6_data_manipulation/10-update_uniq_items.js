50e6ccaa630250b336a3fdb0b6c2beec5ea8f5ee
FRIDAUS <fridausokoya@gmail.com>
2024-10-08 09:11:15 +0100

    update unique values with initial value of 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,10 @@
 export default function updateUniqueItems(map) {
   map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
+    try {
+      if (value === 1) map.set(key, 100);
+    } catch (err) {
+      throw new Error('Cannot process');
+    }
   });
   return map;
 }
