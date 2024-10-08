b647e7d08818008592447c0a54573416a2cb606d
FRIDAUS <fridausokoya@gmail.com>
2024-10-08 10:07:53 +0100

    weakMap implementation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+  if (typeof weakMap.get(endpoint) === 'undefined') {
+    weakMap.set(endpoint, 1);
+    return;
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
