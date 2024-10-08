7637cf5ee3be0456389505a033d372977d38becb
FRIDAUS <fridausokoya@gmail.com>
2024-10-08 08:23:15 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,4 @@
 export default function cleanSet(set, start) {
-  if (start === '') return '';
   const arr = [];
   for (const text of set) {
     if (text.startsWith(start)) arr.push(text.slice(start.length));
