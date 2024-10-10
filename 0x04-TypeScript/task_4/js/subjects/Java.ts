66926e7ff41bccd3239ddafa07d508fde35d4e52
FRIDAUS <fridausokoya@gmail.com>
2024-10-10 15:52:45 +0100

    namespacing and declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,20 @@
+namespace Subjects {
+	export interface Teacher {
+		experienceTeachingJava?: number;
+	}
+	export class Java extends Subject {
+		constructor(teacher: Teacher){
+			super(teacher);
+		}
+		getRequirements(): string {
+			return 'Here is the list of requirements for Java';
+		}
+
+		getAvailableTeacher() {
+			if (!this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+				return 'No available teacher';
+			}
+			return `Available Teacher: ${this.teacher.firstName}`;
+		}
+	}
+}
