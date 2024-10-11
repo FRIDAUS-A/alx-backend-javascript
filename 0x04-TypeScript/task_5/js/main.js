2ed339d58f1d3fd3981f2f317f93b6c07c9831df
FRIDAUS <fridausokoya@gmail.com>
2024-10-11 08:10:04 +0100

    Brand convention & Nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.js
@@ -0,0 +1,60 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (Number(salary) < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    }
+    else {
+        console.log(employee.workTeacherTasks());
+    }
+}
+function teachClass(todayClass) {
+    if (todayClass === 'Math') {
+        console.log('Teaching Math');
+    }
+    else if (todayClass === 'History') {
+        console.log('Teaching History');
+    }
+}
+console.log(createEmployee(200));
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+teachClass('Math');
+teachClass('History');
